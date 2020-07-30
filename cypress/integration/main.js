3515321f2f2159a5e8c10d9068017a945385f6a3
Angel Peña <jose.angpc@gmail.com>
2020-07-30 15:47:55 -0500

    Cypress is embbeded in the app

--- cypress/integration/main.js
+++ cypress/integration/main.js
@@ -1,3 +1,5 @@
+import data from '../../src/books.json'
+
 Cypress.on('uncaught:exception', (err, runnable) => {
     // returning false here prevents Cypress from
     // failing the test
@@ -7,8 +9,10 @@ Cypress.on('uncaught:exception', (err, runnable) => {
 
 
     describe("Verify tha availability...", function() {
+        const url = data[0].author;
       it("Should try to buy a pair of shoes", function() {
-        cy.visit("https://www.nike.com/mx/launch/t/benassi-stussy-off-noir");
+        cy.visit(url);
         cy.wait(10000);
+        cy.scrollTo(0, 500);
       });
     });
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const app = require('./app');
+const cypress = require('cypress');
+const cron = require('node-cron');
 
 async function main () {
     await app.listen(app.get('port'));
@@ -6,3 +8,24 @@ async function main () {
 };
 
 main();
+
+var task = cron.schedule("46 15 * 7 *", function() {
+    console.log("schedule running...");
+    cypress.run ({
+        spec: './cypress/integration/main.js',
+        //spec: specs[`${specStored}`],
+        headless: true,
+        //browser: "electron",
+        browser: "chrome",
+        config: {
+          video: true,
+          baseUrl: 'http://localhost:5000'
+          //video: true
+        }
+      }).then((results) => {
+          console.log(results);
+      }).catch((err) => {
+          console.log(err);
+      })
+    //cypress.open();
+});
